f5523102234c4c2e34d11eb3941a815a2b52cb98
backstabslash <mr.toppanamor1@gmail.com>
2023-02-23 14:16:46 +0200

    fixes

--- src/components/shipment/shipment.jsx
+++ src/components/shipment/shipment.jsx
@@ -1,59 +1,119 @@
-import React, { useRef } from 'react';
-import './shipment.scss'
+import React, { useEffect, useRef } from "react";
+import "./shipment.scss";
 
-const Shipment = ({ openShipment, setOpenShipment, shipment, onEditHandler }) => {
-    const dateInputRef = useRef(null);
-    const orderInputRef = useRef(null);
-    const customerInputRef = useRef(null);
-    const trackingNoInputRef = useRef(null);
-    const statusInputRef = useRef(null);
-    const consigneeInputRef = useRef(null);
-    if (openShipment === true) document.querySelector('body').style.overflow = "hidden";
-    else document.querySelector('body').style.overflow = "visible";
-    return (
-        <div className={`overlay animated ${openShipment ? 'show' : ''}`} onClick={() => setOpenShipment(false)} >
-            <div className="modal" onClick={e => e.stopPropagation()} >
-                <div className='headWrapper'>
-                    <div className='headerTDiv'>Shipment</div>
-                    <div className='headerIDiv'>
-                        <i className="fa fa-times" aria-hidden="true" onClick={() => setOpenShipment(false)}></i>
-                    </div>
-                </div>
-                <div className='wrapper'>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>Date</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.date} ref={dateInputRef}></input>
-                    </div>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>OrderNo</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.orderNo} ref={orderInputRef}></input>
-                    </div>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>Customer</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.customer} ref={customerInputRef}></input>
-                    </div>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>TrackingNo</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.trackingNo} ref={trackingNoInputRef}></input>
-                    </div>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>Status</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.status} ref={statusInputRef}></input>
-                    </div>
-                    <div className='shipContainer'>
-                        <label className='modal--label'>Consignee</label>
-                        <input className='modal--input' type="text" defaultValue={shipment.consignee} ref={consigneeInputRef}></input>
-                    </div>
-                </div>
-                <div className='btnWrapper'>
-                    <button className='btn' onClick={() => {
-                        onEditHandler(shipment.orderNo, orderInputRef.current.value, dateInputRef.current.value, customerInputRef.current.value,
-                            trackingNoInputRef.current.value, statusInputRef.current.value, consigneeInputRef.current.value);
-                        setOpenShipment(false);
-                    }}>Submit <i className="fa-solid fa-square-check"></i></button></div>
-            </div>
-        </div >
-    );
-}
+const Shipment = ({
+  openShipment,
+  setOpenShipment,
+  shipment,
+  onEditHandler,
+}) => {
+  const dateInputRef = useRef(null);
+  const orderInputRef = useRef(null);
+  const customerInputRef = useRef(null);
+  const trackingNoInputRef = useRef(null);
+  const statusInputRef = useRef(null);
+  const consigneeInputRef = useRef(null);
+  useEffect(() => {
+    if (shipment) {
+      dateInputRef.current.value = shipment.date;
+      orderInputRef.current.value = shipment.orderNo;
+      customerInputRef.current.value = shipment.customer;
+      trackingNoInputRef.current.value = shipment.trackingNo;
+      statusInputRef.current.value = shipment.status;
+      consigneeInputRef.current.value = shipment.consignee;
+    }
+  }, [shipment]);
+  //   if (openShipment === true)
+  //     document.querySelector("body").style.overflow = "hidden";
+  //   else document.querySelector("body").style.overflow = "visible";
+  return (
+    <div
+      className={`overlay animated ${openShipment ? "show" : ""}`}
+      onClick={() => setOpenShipment(false)}
+    >
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="headWrapper">
+          <div className="headerTDiv">Shipment</div>
+          <div className="headerIDiv">
+            <i
+              className="fa fa-times"
+              aria-hidden="true"
+              onClick={() => setOpenShipment(false)}
+            ></i>
+          </div>
+        </div>
+        <div className="wrapper">
+          <div className="shipContainer">
+            <label className="modal--label">Date</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={dateInputRef}
+            ></input>
+          </div>
+          <div className="shipContainer">
+            <label className="modal--label">OrderNo</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={orderInputRef}
+            ></input>
+          </div>
+          <div className="shipContainer">
+            <label className="modal--label">Customer</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={customerInputRef}
+            ></input>
+          </div>
+          <div className="shipContainer">
+            <label className="modal--label">TrackingNo</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={trackingNoInputRef}
+            ></input>
+          </div>
+          <div className="shipContainer">
+            <label className="modal--label">Status</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={statusInputRef}
+            ></input>
+          </div>
+          <div className="shipContainer">
+            <label className="modal--label">Consignee</label>
+            <input
+              className="modal--input"
+              type="text"
+              ref={consigneeInputRef}
+            ></input>
+          </div>
+        </div>
+        <div className="btnWrapper">
+          <button
+            className="btn"
+            onClick={() => {
+              onEditHandler(
+                shipment.orderNo,
+                orderInputRef.current.value,
+                dateInputRef.current.value,
+                customerInputRef.current.value,
+                trackingNoInputRef.current.value,
+                statusInputRef.current.value,
+                consigneeInputRef.current.value
+              );
+              setOpenShipment(false);
+            }}
+          >
+            Submit <i className="fa-solid fa-square-check"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Shipment;
--- src/components/shipments/shipments.jsx
+++ src/components/shipments/shipments.jsx
@@ -1,95 +1,135 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import './shipments.css'
-import Shipment from '../shipment/shipment';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./shipments.css";
+import Shipment from "../shipment/shipment";
 
 const Shipments = () => {
-    const [shipments, setShipments] = useState([]);
-    const [shipment, setShipment] = useState({});
-    const [openShipment, setOpenShipment] = useState(false);
-    const [dateSort, setDateSort] = useState(0); //0 off 1asc 2desc
-    useEffect(() => {
-        axios.get("https://my.api.mockaroo.com/shipments.json?key=5e0b62d0")
-            .then((res) => { setShipments(res.data); })
-            .catch((err) => {
-                console.log(err);
-                axios.get("http://localhost:3000/Shipments.txt")
-                    .then((res) => {
-                        setShipments(res.data);
-                    })
-                    .catch((err) => console.log(err));
-            })
-    }, []);
-    const deleteHandler = (e) => {
-        setShipments([...shipments.filter(shipment => shipment.orderNo !== e)]);
+  const [shipments, setShipments] = useState([]);
+  const [shipment, setShipment] = useState({});
+  const [openShipment, setOpenShipment] = useState(false);
+  const [dateSort, setDateSort] = useState(0); // 0off 1asc 2desc
+  useEffect(() => {
+    axios
+      .get("https://my.api.mockaroo.com/shipments.json?key=5e0b62d0")
+      .then((res) => {
+        setShipments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        axios
+          .get("http://localhost:3000/Shipments.txt")
+          .then((res) => {
+            setShipments(res.data);
+          })
+          .catch((err) => console.log(err));
+      });
+  }, []);
+  const deleteHandler = (e) => {
+    setShipments([...shipments.filter((shipment) => shipment.orderNo !== e)]);
+  };
+  const onEditHandler = (
+    prevOrderNo,
+    orderNo,
+    date,
+    customer,
+    trackingNo,
+    status,
+    consignee
+  ) => {
+    for (let i = 0; i < shipments.length; i++) {
+      if (shipments[i].orderNo === prevOrderNo) {
+        let copyShip = [...shipments];
+        copyShip[i] = {
+          orderNo,
+          date,
+          customer,
+          trackingNo,
+          status,
+          consignee,
+        };
+        setShipments([...copyShip]);
+      }
     }
-    const onEditHandler = (prevOrderNo, orderNo, date, customer, trackingNo, status, consignee) => {
-        for (let i = 0; i < shipments.length; i++) {
-            if (shipments[i].orderNo === prevOrderNo) {
-                let copyShip = [...shipments];
-                copyShip[i] = { orderNo, date, customer, trackingNo, status, consignee }
-                setShipments([...copyShip])
-            }
-        }
+  };
+  const dateSortHandler = () => {
+    if (dateSort === 0) {
+      let copyShip = [...shipments];
+      copyShip.sort(function (a, b) {
+        return Date.parse(a.date) - Date.parse(b.date);
+      });
+      setShipments(copyShip);
+      setDateSort(1);
     }
-    const dateSortHandler = () => {
-        if (dateSort === 0) {
-            let copyShip = [...shipments];
-            copyShip.sort(function (a, b) {
-                return Date.parse(a.date) - Date.parse(b.date);
-            });
-            setShipments(copyShip);
-            setDateSort(1);
-        }
-        if (dateSort === 1) {
-            let copyShip = [...shipments];
-            copyShip.sort(function (a, b) {
-                return Date.parse(b.date) - Date.parse(a.date);
-            });
-            setShipments(copyShip);
-            setDateSort(2);
-        }
-        if (dateSort === 2) setDateSort(0);
+    if (dateSort === 1) {
+      let copyShip = [...shipments];
+      copyShip.sort(function (a, b) {
+        return Date.parse(b.date) - Date.parse(a.date);
+      });
+      setShipments(copyShip);
+      setDateSort(2);
     }
-    return (
-        <>
-            <header className='headerWrapper' >
-                <div className='titleDiv'>
-                    Shipments
-                </div></header>
-            <div className="wrapper" >
-                <Shipment openShipment={openShipment} setOpenShipment={setOpenShipment} shipment={shipment} onEditHandler={onEditHandler} />
-                <table className='app--table'>
-                    <thead className='app--thead'>
-                        <tr className='app--tr'>
-                            <th className='app--th'>orderNo</th>
-                            <th className='app--th-date' onClick={dateSortHandler}>date</th>
-                            <th className='app--th'>customer</th>
-                            <th className='app--th'>trackingNo</th>
-                            <th className='app--th'>status</th>
-                            <th className='app--th'>consignee</th>
-                            <th className='app--th'></th>
-                            <th className='app--th'></th>
-                        </tr>
-                    </thead>
-                    <tbody className='app-tbody'>
-                        {shipments.map(shipment => (
-                            <tr className='app--tr' key={shipment.orderNo}>
-                                <td className='app--td'>{shipment.orderNo}</td>
-                                <td className='app--td'>{shipment.date}</td>
-                                <td className='app--td'>{shipment.customer}</td>
-                                <td className='app--td'>{shipment.trackingNo}</td>
-                                <td className='app--td'>{shipment.status}</td>
-                                <td className='app--td'>{shipment.consignee}</td>
-                                <td className='app--td-button'> <button className='app--button-delete' onClick={() => deleteHandler(shipment.orderNo)}><i className="fa-solid fa-square-xmark"></i></button></td>
-                                <td className='app--td-button'> <button className='app--button-ship' onClick={() => { setOpenShipment(true); setShipment(shipment); }}><i className="fa-sharp fa-solid fa-address-card"></i></button></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    );
-}
+    if (dateSort === 2) setDateSort(0);
+  };
+  return (
+    <>
+      <header className="headerWrapper">
+        <div className="titleDiv">Shipments</div>
+      </header>
+      <Shipment
+        openShipment={openShipment}
+        setOpenShipment={setOpenShipment}
+        shipment={shipment}
+        onEditHandler={onEditHandler}
+      />
+      <table className="app--table">
+        <thead className="app--thead">
+          <tr className="app--tr">
+            <th className="app--th">orderNo</th>
+            <th className="app--th-date" onClick={dateSortHandler}>
+              date
+            </th>
+            <th className="app--th">customer</th>
+            <th className="app--th">trackingNo</th>
+            <th className="app--th">status</th>
+            <th className="app--th">consignee</th>
+            <th className="app--th"></th>
+            <th className="app--th"></th>
+          </tr>
+        </thead>
+        <tbody className="app-tbody">
+          {shipments.map((shipment) => (
+            <tr className="app--tr" key={shipment.orderNo}>
+              <td className="app--td">{shipment.orderNo}</td>
+              <td className="app--td">{shipment.date}</td>
+              <td className="app--td">{shipment.customer}</td>
+              <td className="app--td">{shipment.trackingNo}</td>
+              <td className="app--td">{shipment.status}</td>
+              <td className="app--td">{shipment.consignee}</td>
+              <td className="app--td-button">
+                <button
+                  className="app--button-delete"
+                  onClick={() => deleteHandler(shipment.orderNo)}
+                >
+                  <i className="fa-solid fa-square-xmark"></i>
+                </button>
+              </td>
+              <td className="app--td-button">
+                <button
+                  className="app--button-ship"
+                  onClick={() => {
+                    setShipment(shipment);
+                    setOpenShipment(true);
+                  }}
+                >
+                  <i className="fa-sharp fa-solid fa-address-card"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Shipments;
